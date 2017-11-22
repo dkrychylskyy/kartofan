38cdd5707d465bdde44e91c78fd6192fe5647ead
dkrychylskyy <d.krychylskyy@gmail.com>
2017-11-22 09:41:57 +0100

    Ajouter NgAnimate, Angular-Motion. Ajouter popover.demo.tpl.html pour afficher les donnes dans une petite fenetre. Changer la catre par style. Ajouter dans la config popoverProvider.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 
-var kartofan = angular.module('kartofan', ['ngMap']);
+var kartofan = angular.module('kartofan', ['ngMap', 'mgcrea.ngStrap', 'ngAnimate']);
+kartofan.config(function($popoverProvider) {
+    angular.extend($popoverProvider.defaults, {
+      html: true
+    });
+  });
+  
