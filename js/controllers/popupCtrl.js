092d7d442dfe67f38918c9291319bda9adb92fc1
dkrychylskyy <d.krychylskyy@gmail.com>
2017-11-24 09:44:39 +0100

    Ajout du prix dans l'objet commande

--- js/controllers/popupCtrl.js
+++ js/controllers/popupCtrl.js
@@ -4,18 +4,26 @@ kartofan.controller('popupCtrl', function popupCtrl(NgMap, $modal, $popover, $sc
 
     popup.plus = function (commande, titre, prix) {
         popup.qte++;
-        commande.set(titre, popup.qte);
+        var qte = popup.qte;
+        var obj = {};
+        obj.qte = qte;
+        obj.prix = prix;
+        commande.set(titre, obj);
         console.log(commande);
     };
 
     popup.minus = function (commande, titre, prix) {
+        var qte = popup.qte;
+        var obj = {};
+        obj.qte = qte-1;
+        obj.prix = prix;
         if (popup.qte == 0) {
             return;
         }
         if (popup.qte == 1) {
-            commande.delete(titre, popup.qte);
+            commande.delete(titre, obj);
         } else {
-            commande.set(titre, (popup.qte - 1));
+            commande.set(titre, obj);
         }
         popup.qte--;
     };
