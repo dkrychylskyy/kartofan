a835d585747e40ca5ae90f2d026ff7d1e6380ebb
dkrychylskyy <d.krychylskyy@gmail.com>
2017-11-24 14:04:03 +0100

    Ajoute dateDeLivraison dans commande

--- js/controllers/commandeCtrl.js
+++ js/controllers/commandeCtrl.js
@@ -11,28 +11,42 @@ kartofan.controller('commandeCtrl', function commandeCtrl(localStorageService) {
     }
 
     /* Convert un Map en un objet classique  */
-    function mapToObj(aMap) {
+    function mapToObj(aMap, delaiDelIvraison) {
             var obj = {};
             aMap.forEach(function (v, k) {
                 obj[k] = v;
             });
+            obj.dateDeLivraison = delaiDelIvraisonToMilisec(delaiDelIvraison);
             return obj;
     }
 
+    /* Convert delai de livraison en milisec */
+    function delaiDelIvraisonToMilisec(delaiDelIvraison) {
+        var delDeLivrMsec =  delaiDelIvraison*60*1000;
+
+    /* Retourne date de livraison en milisec */
+        var date = new Date();
+        var currentTime = date.getTime();
+        console.log(currentTime);
+        var dateDeLivraison = currentTime + delDeLivrMsec;
+        console.log(dateDeLivraison);
+        return dateDeLivraison;
+    }
+
     /* Ajoute le commande dans localStorage */
-    function addCommandeInLocalStorege(params) {
+    function addCommandeInLocalStorage(params, delaiDelIvraison) {
         if(localStorageService.isSupported) {
             localStorageService.setPrefix('comm');
             var key = genIdUniq();
-            var commande = mapToObj(params);
+            var commande = mapToObj(params, delaiDelIvraison);
             localStorageService.set(key, commande);
           }
     }
 
     /* Recupere le commande depuis controller */
-    commCtrl.commander = function(commande) {
+    commCtrl.commander = function(commande, delaiDelIvraison) {
         commCtrl.commande = commande;
-        addCommandeInLocalStorege(commande);
+        addCommandeInLocalStorage(commande, delaiDelIvraison);
         return commCtrl.commande;
     };
 });
