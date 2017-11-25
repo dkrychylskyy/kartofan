668a805c652193be4e2fc4e7c9a0be1bea3b3ff6
Henoc <henoc.nzuzi@gmail.com>
2017-11-25 19:49:02 +0100

    La connexion sur la page index est fonctionnelle

--- js/controllers/googlePlusCtrl.js
+++ js/controllers/googlePlusCtrl.js
@@ -1,29 +1,43 @@
-kartofan.controller('AuthCtrl', ['$scope', 'GooglePlus', 'localStorageService', function ($scope, GooglePlus,localStorageService) {
+kartofan.controller('AuthCtrl', ['$scope', 'GooglePlus', 'localStorageService', function ($scope, GooglePlus, localStorageService) {
+    auth = this;
     $scope.login = function () {
         GooglePlus.login().then(function (authResult) {
             console.log(authResult);
- 
+
             GooglePlus.getUser().then(function (user) {
                 console.log(user);
                 localStorageService.set(user.id, user);
-                
+
             });
         }, function (err) {
             console.log(err);
         });
     };
-
-    var email = document.getElementById("emailfield").value;
-    var password = document.getElementById("passwordfield").value;
     db.createIndex({
-        index: {fields: ['_id']}
-      });
-    
-    var test = db.find({
-        selector: {
-          _id: 'henoc.nzuzi@gmail.com'
+        index: {
+            fields: ['_id', 'Mot de passe']
         }
-      });
-    console.log(test);
+    });
+    auth.connect = function() {
+        var email = document.getElementById("emailfield").value;
+        var password = document.getElementById("passwordfield").value;
+        console.log(email);
+        console.log(password);
+        var test = db.find({
+            selector: {
+                _id: email,
+                "Mot de passe": password
+            }
+        }).then(function (result) {
+            if (result.bookmark !== "nil") {
+                window.location.assign("mapactivity.html");
+            } else {
+                alert('Adresse mail ou mot de passe erroné.');
+            }
+        }).catch(function (err) {
+            console.log(err);
+        });
+    };
+    /* console.log(test); */
 
 }]);
