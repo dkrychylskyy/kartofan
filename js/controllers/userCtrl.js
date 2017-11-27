06f75203af2b245f249229edccebea57fc9cf81b
Henoc <henoc@github.com>
2017-11-27 15:20:33 +0100

    changement mineurs pr version finale

--- js/controllers/userCtrl.js
+++ js/controllers/userCtrl.js
@@ -1,20 +1,15 @@
 /*jshint esversion: 6*/
 kartofan.controller('userCtrl', ['$scope', 'GooglePlus', '$interval', function ($scope, GooglePlus, $interval, $element, $window) {
     user = this;
-
-    function millisToMinutesAndSeconds(millis) {
-        var minutes = Math.floor(millis / 60000);
-        var seconds = ((millis % 60000) / 1000).toFixed(0);
-        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-      }
-
+    
     function round(value, decimals) {
         return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
     }
 
     user.info = function () {
         var url = window.location.toString();
-        var email = url.slice(48);
+        var mail = url.indexOf('#', 0);
+        var email = url.slice(mail+1);
         var comm = db.find({
             selector: {
                 "obj.id_user": {
@@ -26,41 +21,16 @@ kartofan.controller('userCtrl', ['$scope', 'GooglePlus', '$interval', function (
             /* sort: ['dateCommande'] */
         }).then(function (result) {
             if (result.bookmark !== "nil") {
+                console.log(result);
                 var comm = JSON.stringify(result);
+                console.log(comm);
                 var comm1 = comm.replace(/{"docs":/gi, " ");
                 var comm2 = comm1.replace(/{"obj":/gi, " ");
                 var comm3 = comm2.replace(/}}/gi, "}");
                 var comm4 = comm3.indexOf('],"', 0);
                 var commandes = comm3.slice(0, [comm4 + 1]);
                 commandes = JSON.parse(commandes);
-                $scope.order = function () {
                     i = 0;
-                    $scope.callAtInterval = function() {
-                        j = 0;
-                        while (commandes[j]) {
-                            var dateLivraison = commandes[j].dateDeLivraison;
-                            var time = new Date();
-                            var currentTime = time.getTime();
-                            var stat = commandes[j].status;
-                            //console.log(dateLivraison);
-                            //console.log(currentTime);
-                            /* while(currentTime < dateLivraison && commandes[j]) {
-                                dateLivraison = millisToMinutesAndSeconds(dateLivraison);
-                                currentTime = millisToMinutesAndSeconds(currentTime);
-                                console.log(dateLivraison);
-                                console.log(currentTime);
-                                angular.element('.status').html("Il reste: " + (dateLivraison - currentTime) + " avant la livraison théorique");
-                                j++;
-                            } */ 
-                            while(dateLivraison < currentTime && commandes[j]) {
-                                angular.element('.status').html("Livrée");
-                                j++;
-                            }
-                            j++;
-                        }
-                        console.log("$scope.callAtInterval - Interval occurred");
-                    };
-                    //$interval( function(){ $scope.callAtInterval(); }, 3000);
                     while (commandes[i]) {
                         var dateDeCommande = commandes[i].dateCommande;
                         var montantHt = 0;
@@ -84,12 +54,9 @@ kartofan.controller('userCtrl', ['$scope', 'GooglePlus', '$interval', function (
                             montantHt += prixHt3;
                             montantTtc += prixTtc3;
                         }
-                        angular.element('<tr><td>' + dateDeCommande + '</td><td>' + montantHt + "€" + '</td><td>' + montantTtc + "€" + '</td><td id="' + i + '">' + status + '</td></tr>').appendTo('#commandes');
-                        $interval($scope.callAtInterval, 2000);
+                        angular.element('<tr><td>' + dateDeCommande + '</td><td>' + montantHt + "€" + '</td><td>' + montantTtc + "€" + '</td></tr>').appendTo('#commandes');
                         i++;
-
                     }
-                };
             } else {
                 alert("Aucune commande pour l'instant...Qu'est ce que vous attendez ?");
             }
